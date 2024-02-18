81cc4f87d02e29fa93e8c8cea513b4315bf3640d
Yan Klyuzhin <klyuzhin@uchi.ru>
2024-02-18 08:31:16 +0200

    refactored drawer

--- game.js
+++ game.js
@@ -2,8 +2,6 @@ const searchParams = new URLSearchParams(window.location.search);
 const taxRate = Number(searchParams.get("tax"));
 
 const scrn = document.getElementById("canvas");
-const sctx = scrn.getContext("2d");
-scrn.tabIndex = 1;
 
 const GAME_DURATION_MS = 60_000;
 
@@ -21,62 +19,69 @@ const throttle = (callback, delay) => {
 
 class Drawer {
   game;
-  colors = {
+  sctx;
+
+  COLORS = {
+    black: "black",
     textGray: "#A2A2A2",
     ballColor: "#DCDCDC",
     lineGray: "#CCCCCC",
+    taxRect: "rgba(233, 170, 170, 0.3)",
+    taxRate: "red",
   };
-  lineWidth_1 = 1;
-  lineWidth_2 = 2;
-  h1Font = "700 28px courier";
-  h2Font = "400 22px Tahoma";
+  BIG_FONT = "700 28px courier";
+  SMALL_FONT = "400 22px Tahoma";
+  DASH_LENGTH = 15;
 
   constructor(game) {
     this.game = game;
+
+    this.sctx = game.scrn.getContext("2d");
   }
 
-  setTextStyles = (h1 = true) => {
-    sctx.font = h1 ? this.h1Font : this.h2Font;
-    sctx.setLineDash([]);
-    sctx.fillStyle = "black";
-    sctx.strokeStyle = "black";
+  clearScreen = () => {
+    const { scrn } = game;
+
+    this.sctx.fillStyle = "white";
+    this.sctx.fillRect(0, 0, scrn.width, scrn.height);
   };
 
-  drawCircle = (x, y, radius) => {
-    sctx.fillStyle = this.colors.ballColor;
-    sctx.beginPath();
-    sctx.arc(x, y, radius, 0, 2 * Math.PI);
-    sctx.fill();
+  drawCircle = (x, y, radius, color) => {
+    this.sctx.fillStyle = this.COLORS[color];
+    this.sctx.beginPath();
+    this.sctx.arc(x, y, radius, 0, 2 * Math.PI);
+    this.sctx.fill();
   };
 
-  drawHorizontalDashedLine = (y, dashOffset) => {
+  drawHorizontalDashedLine = (y, dashOffset, color, lineWidth) => {
     const { DISPLAY_WITDH } = this.game.ui;
 
-    sctx.lineDashOffset = dashOffset;
-    sctx.lineWidth = this.lineWidth_1;
-    sctx.beginPath();
-    sctx.setLineDash([15, 15]);
-    sctx.moveTo(0, y);
-    sctx.lineTo(DISPLAY_WITDH, y);
-    sctx.strokeStyle = this.colors.lineGray;
-    sctx.stroke();
+    this.sctx.lineDashOffset = dashOffset;
+    this.sctx.lineWidth = lineWidth;
+    this.sctx.strokeStyle = this.COLORS[color];
+
+    this.sctx.beginPath();
+    this.sctx.setLineDash([this.DASH_LENGTH, this.DASH_LENGTH]);
+    this.sctx.moveTo(0, y);
+    this.sctx.lineTo(DISPLAY_WITDH, y);
+    this.sctx.stroke();
   };
 
-  drawLine = (last200Coords) => {
-    sctx.setLineDash([]);
-    sctx.lineWidth = this.lineWidth_2;
-    sctx.beginPath();
-    if (last200Coords.length < 2) return;
-    for (let i = 0; i < last200Coords.length - 1; i++) {
-      const lastPoint = last200Coords[i];
-      const firstPoint = last200Coords[i + 1];
+  drawCurvedLine = (coords, lineWidth) => {
+    this.sctx.setLineDash([]);
+    this.sctx.lineWidth = lineWidth;
+
+    this.sctx.beginPath();
+    for (let i = 0; i < coords.length - 1; i++) {
+      const lastPoint = coords[i];
+      const firstPoint = coords[i + 1];
 
       const x_mid = (lastPoint.x + firstPoint.x) / 2;
       const y_mid = (lastPoint.y + firstPoint.y) / 2;
       const cp_x = (x_mid + lastPoint.x) / 2;
       const cp_y = (y_mid + lastPoint.y) / 2;
 
-      sctx.quadraticCurveTo(cp_x, cp_y, x_mid, y_mid);
+      this.sctx.quadraticCurveTo(cp_x, cp_y, x_mid, y_mid);
     }
     // const isTaxIntersection = firstPoint.x < tax.taxes[0].x && firstPoint.y > tax.taxes[0].y;
     // sctx.strokeStyle = isTaxIntersection ? "red" : "black";
@@ -88,27 +93,54 @@ class Drawer {
     // gradient.addColorStop(1, "red");
 
     // sctx.strokeStyle = pointXCoord < tax.taxes[0].x ? "black" : gradient;
-    sctx.strokeStyle = "black";
-    sctx.stroke();
+    this.sctx.strokeStyle = this.COLORS.black;
+    this.sctx.stroke();
     // sctx.save();
   };
 
-  drawDashedCrosshair = (x, y) => {
+  drawDashedCrosshair = (x, y, color) => {
     const { FLOOR_Y, CEILING_Y, DISPLAY_WITDH } = this.game.ui;
 
-    sctx.lineDashOffset = 0;
-    sctx.beginPath();
-    sctx.setLineDash([15, 15]);
+    this.sctx.lineDashOffset = 0;
+    this.sctx.setLineDash([15, 15]);
+    this.sctx.strokeStyle = this.COLORS[color];
 
-    sctx.moveTo(x, CEILING_Y);
-    sctx.lineTo(x, FLOOR_Y);
+    this.sctx.beginPath();
 
-    sctx.moveTo(0, y);
-    sctx.lineTo(DISPLAY_WITDH, y);
+    this.sctx.moveTo(x, CEILING_Y);
+    this.sctx.lineTo(x, FLOOR_Y);
 
-    sctx.strokeStyle = this.colors.lineGray;
-    sctx.stroke();
+    this.sctx.moveTo(0, y);
+    this.sctx.lineTo(DISPLAY_WITDH, y);
+
+    this.sctx.stroke();
     // sctx.save();
+  };
+
+  drawRectangle = (x, y, width, height, color) => {
+    this.sctx.fillStyle = this.COLORS[color];
+    this.sctx.fillRect(x, y, width, height);
+  };
+
+  drawText = ({ text, x, y, color, isBig = false, align = "left" }) => {
+    this.sctx.font = isBig ? this.BIG_FONT : this.SMALL_FONT;
+    this.sctx.setLineDash([]);
+
+    if (align) {
+      this.sctx.textAlign = align;
+    }
+
+    if (color) {
+      this.sctx.fillStyle = this.COLORS[color];
+    } else {
+      this.sctx.fillStyle = this.COLORS.black;
+    }
+
+    this.sctx.fillText(text, x, y);
+  };
+
+  drawImage = (image, x, y) => {
+    this.sctx.drawImage(image, x, y);
   };
 }
 
@@ -128,19 +160,29 @@ class Taxing {
   };
 
   draw = () => {
-    const { ui, currentGameStep, playGameStep, framesAmount, TICK_SHIFT_X } =
-      this.game;
+    const {
+      ui,
+      drawer,
+      currentGameStep,
+      playGameStep,
+      framesAmount,
+      TICK_SHIFT_X,
+    } = this.game;
     const { DISPLAY_WITDH } = ui;
 
     for (let tax of this.taxes) {
-      sctx.fillStyle = "rgba(233, 170, 170, 0.3)";
       const taxWidth = DISPLAY_WITDH - tax.x;
       const taxHeight =
         this.getYByScorePerSecond(0) - this.getYByScorePerSecond(tax.taxRate);
-      sctx.fillRect(tax.x, tax.y, taxWidth, taxHeight);
+      drawer.drawRectangle(tax.x, tax.y, taxWidth, taxHeight, "taxRect");
+
       if (tax.x < 900) {
-        sctx.fillStyle = "red";
-        sctx.fillText(`${tax.taxRate} ¢`, 940, tax.y + taxRate);
+        drawer.drawText({
+          text: `${tax.taxRate} ¢`,
+          x: 940,
+          y: tax.y + tax.taxRate,
+          color: "taxRate",
+        });
       }
     }
 
@@ -184,25 +226,13 @@ class Ball {
     return result;
   };
 
-  drawHeight = (x, y) => {
-    const { drawer } = this.game;
-
-    drawer.setTextStyles(true);
-
-    let scorePerSecond = this.getScorePerSecondByY(y);
-    sctx.textAlign = "left";
-    sctx.fillText(scorePerSecond, x + 45, y - 10);
-
-    this.game.scorePerSecond = scorePerSecond;
-  };
-
   draw = () => {
     const { drawer, currentGameStep, playGameStep, ui, TICK_SHIFT_X } =
       this.game;
     const { FLOOR_Y, CEILING_Y } = ui;
 
     if (currentGameStep !== playGameStep) {
-      drawer.drawCircle(this.X, this.y, this.RADIUS);
+      drawer.drawCircle(this.X, this.y, this.RADIUS, "ballColor");
       return;
     }
 
@@ -233,12 +263,24 @@ class Ball {
       return { x: coords.x - TICK_SHIFT_X, y: coords.y, date: coords.date };
     });
     this.coordsHistory.push({ x: this.X, y: this.y, date: Date.now() });
-    drawer.drawDashedCrosshair(this.X, this.y);
+    drawer.drawDashedCrosshair(this.X, this.y, "lineGray");
 
-    drawer.drawCircle(this.X, this.y, this.RADIUS);
-    drawer.drawLine(this.coordsHistory);
+    drawer.drawCircle(this.X, this.y, this.RADIUS, "ballColor");
 
-    this.drawHeight(this.X, this.y);
+    if (this.coordsHistory.length > 1) {
+      drawer.drawCurvedLine(this.coordsHistory, 2);
+    }
+
+    let scorePerSecond = this.getScorePerSecondByY(this.y);
+    this.game.scorePerSecond = scorePerSecond;
+    drawer.drawText({
+      text: scorePerSecond,
+      x: this.X + 45,
+      y: this.y - 10,
+      isBig: true,
+      align: "left",
+    });
+
     this.checkIsTaxIntersection();
   };
 
@@ -285,15 +327,21 @@ class UI {
     this.tapImages[1].src = "img/tap2.png";
   }
 
-  clearScreen = () => {
-    sctx.fillRect(0, 0, scrn.width, scrn.height);
-  };
-
   drawBorders = () => {
     const { drawer, displayPositionX } = this.game;
 
-    drawer.drawHorizontalDashedLine(this.FLOOR_Y, -displayPositionX);
-    drawer.drawHorizontalDashedLine(this.CEILING_Y, -displayPositionX);
+    drawer.drawHorizontalDashedLine(
+      this.FLOOR_Y,
+      -displayPositionX,
+      "lineGray",
+      1
+    );
+    drawer.drawHorizontalDashedLine(
+      this.CEILING_Y,
+      -displayPositionX,
+      "lineGray",
+      1
+    );
   };
 
   draw = () => {
@@ -312,86 +360,130 @@ class UI {
   };
 
   drawGetReady = () => {
+    const { drawer, scrn } = this.game;
+
     const x = parseFloat(scrn.width - this.getReadyImage.width) / 2;
     const y = parseFloat(scrn.height - this.getReadyImage.height) / 2;
     const width = parseFloat(scrn.width - this.tapImages[0].width) / 2;
     const height = y + this.getReadyImage.height - this.tapImages[0].height;
-    sctx.drawImage(this.getReadyImage, x, y);
-    sctx.drawImage(this.tapImages[this.frameIndex], width, height);
+
+    drawer.drawImage(this.getReadyImage, x, y);
+    drawer.drawImage(this.tapImages[this.frameIndex], width, height);
   };
 
   drawGameOver = () => {
+    const { drawer } = this.game;
+
     const x = parseFloat(scrn.width - this.gameOverImage.width) / 2;
     const y = parseFloat(scrn.height - this.gameOverImage.height) / 2;
     // this.tx = parseFloat(scrn.width - this.tapImages[0].width) / 2;
     // this.ty = this.y + this.gameOverImage.height - this.tapImages[0].height;
-    sctx.fillStyle = "white";
+    // sctx.fillStyle = "white";
     // sctx.fillRect(this.x - 150, this.y - 80, 400, 200);
-    sctx.drawImage(this.gameOverImage, x, y);
+    drawer.drawImage(this.gameOverImage, x, y);
   };
 
   drawProducedScore = () => {
-    const { drawer, scoreProduced } = this.game;
+    const { drawer, scrn, scoreProduced } = this.game;
 
-    drawer.setTextStyles(false);
-    sctx.textAlign = "right";
-    sctx.fillText(scoreProduced + "¢", scrn.width - 150, 40);
-    sctx.textAlign = "left";
-    sctx.fillStyle = drawer.colors.textGray;
-    sctx.fillText("produced", scrn.width - 140, 40);
+    drawer.drawText({
+      text: `${scoreProduced}¢`,
+      x: scrn.width - 150,
+      y: 40,
+      isBig: false,
+      align: "right",
+    });
+    drawer.drawText({
+      text: "produced",
+      x: scrn.width - 140,
+      y: 40,
+      color: "textGray",
+      align: "left",
+    });
   };
 
   drawTaxed = () => {
-    const { drawer, scoreTaxed } = this.game;
+    const { drawer, scrn, scoreTaxed } = this.game;
 
-    drawer.setTextStyles(false);
-    sctx.textAlign = "right";
-    sctx.fillText(scoreTaxed + "¢", scrn.width - 150, 70);
-    sctx.fillStyle = drawer.colors.textGray;
-    sctx.textAlign = "left";
-    sctx.fillText("taxed", scrn.width - 140, 70);
+    drawer.drawText({
+      text: `${scoreTaxed}¢`,
+      x: scrn.width - 150,
+      y: 70,
+      isBig: false,
+      align: "right",
+    });
+    drawer.drawText({
+      text: "taxed",
+      x: scrn.width - 140,
+      y: 70,
+      color: "textGray",
+      align: "left",
+    });
   };
 
   drawProfitScore = () => {
     const { drawer, scoreProduced, scoreTaxed } = this.game;
 
-    drawer.setTextStyles(false);
-    sctx.textAlign = "right";
-    sctx.fillText(scoreProduced + scoreTaxed + "¢", 500, 40);
-    sctx.textAlign = "left";
-    sctx.fillStyle = drawer.colors.textGray;
-    sctx.fillText("profit", 510, 40);
+    drawer.drawText({
+      text: `${scoreProduced + scoreTaxed}¢`,
+      x: 500,
+      y: 40,
+      isBig: false,
+      align: "right",
+    });
+    drawer.drawText({
+      text: "profit",
+      x: 510,
+      y: 40,
+      color: "textGray",
+      align: "left",
+    });
   };
 
   drawProfitPerSecond = () => {
     const { drawer, scorePerSecond, taxPerSecond } = this.game;
 
-    drawer.setTextStyles(false);
-    sctx.textAlign = "right";
-    sctx.fillText(scorePerSecond - taxPerSecond + "¢", 500, 70);
-    sctx.textAlign = "left";
-    sctx.fillStyle = drawer.colors.textGray;
-    sctx.fillText("profit /s", 510, 70);
+    drawer.drawText({
+      text: `${scorePerSecond - taxPerSecond}¢`,
+      x: 500,
+      y: 70,
+      isBig: false,
+      align: "right",
+    });
+    drawer.drawText({
+      text: "profit/s",
+      x: 510,
+      y: 70,
+      color: "textGray",
+      align: "left",
+    });
   };
 
   drawTimer = () => {
     const { drawer, leftTimerValue } = this.game;
 
     if (!leftTimerValue) this.game.setLeftTimerValue();
-    drawer.setTextStyles(false);
-    sctx.textAlign = "right";
-    sctx.fillText(leftTimerValue?.toLocaleTimeString("en-GB") + " /", 200, 70);
-    sctx.textAlign = "left";
+
+    drawer.drawText({
+      text: `${leftTimerValue?.toLocaleTimeString("en-GB")} /`,
+      x: 200,
+      y: 70,
+      align: "right",
+    });
+
     const rightTimer = new Date(2000, 0, 0, 1, 0, 0).toLocaleTimeString(
       "en-GB"
     );
-    sctx.fillText(rightTimer, 208, 70);
+
+    drawer.drawText({
+      text: rightTimer,
+      x: 208,
+      y: 70,
+      align: "left",
+    });
   };
 
   drawScore = () => {
-    const { drawer } = this.game;
-
-    drawer.setTextStyles();
     this.drawProducedScore();
     this.drawProfitScore();
     this.drawTaxed();
@@ -467,6 +559,8 @@ class Game {
   ball;
   ui;
 
+  scrn;
+
   duration = 0;
   interval = 0;
   framesAmount = 0;
@@ -495,7 +589,9 @@ class Game {
     profit: 0,
   };
 
-  constructor(duration) {
+  constructor(scrn, duration) {
+    this.scrn = scrn;
+
     this.duration = duration;
 
     this.drawer = new Drawer(this);
@@ -503,7 +599,8 @@ class Game {
     this.ui = new UI(this);
     this.ball = new Ball(this);
 
-    scrn.addEventListener("click", () => {
+    this.scrn.tabIndex = 1;
+    this.scrn.addEventListener("click", () => {
       switch (this.currentGameStep) {
         case this.indexGameStep:
           this.currentGameStep = this.playGameStep;
@@ -532,8 +629,7 @@ class Game {
   };
 
   draw = () => {
-    sctx.fillStyle = "white";
-    this.ui.clearScreen();
+    this.drawer.clearScreen();
     this.taxing.draw();
     this.ui.drawBorders();
 
@@ -566,6 +662,6 @@ class Game {
   }, 1000);
 }
 
-const game = new Game(GAME_DURATION_MS);
+const game = new Game(scrn, GAME_DURATION_MS);
 
 game.run();
